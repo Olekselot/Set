62c967174af362942f23654e1e8af5dbc63da80b
Yuriy Fundak <yfundak@gmail.com>
2024-03-27 11:53:52 +0200

    Added unit tests project. Currently without implementation

---
+++ SetTest/SetTest.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace SetTest
+{
+	TEST_CLASS(SetTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+	};
+}
