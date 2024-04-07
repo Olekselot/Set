974db19ea0a5ac750a171285854794b7ed3de383
Юрій Фундак <yfundak@gmail.com>
2024-04-07 19:12:50 +0300

    2 new functions

--- Set/functions.cpp
+++ Set/functions.cpp
@@ -1,8 +1,8 @@
 #include "set.h"
 
-enum Product
+enum class Product
 {
-	Bread = 1,
+	Bread,
 	Butter,
 	Milk,
 	Cheese,
@@ -14,3 +14,33 @@ enum Product
 	Coffee,
 	Water
 };
+
+//5.1 Фундак Ю.
+Set<Product> products_intersection(Set<Product>* shops, size_t nof_shops)
+{
+	if (nof_shops == 0)
+	{
+		return Set<Product>();
+	}
+	Set<Product> common_products = shops[0];
+
+	for (size_t i = 1; i < nof_shops; ++i)
+	{
+		common_products = common_products.set_intersect(shops[i]);
+	}
+
+	return common_products;
+}
+
+//5.2 Фундак Ю.
+Set<Product> products_union(Set<Product>* shops, size_t nof_shops)
+{
+	Set<Product> any_products;
+
+	for (size_t i = 0; i < nof_shops; ++i)
+	{
+		any_products = any_products.set_union(shops[i]);
+	}
+
+	return any_products;
+}
